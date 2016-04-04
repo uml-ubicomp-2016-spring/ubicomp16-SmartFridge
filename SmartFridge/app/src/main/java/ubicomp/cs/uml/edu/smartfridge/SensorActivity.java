e971467e3917eb147f96b21907f20ba0ee1771b9
vdhamoda <Ultron@Vigneshs-MacBook-Pro.local>
2016-04-03 22:31:04 -0400

    Added Euler angle based door status detection
    Reduced the Sensor Data Noise to give accurate reading. Converted
    Magnetometer Reading to Euler’s angles and Calculated a door open
    position using sensor data comparison over 10 trials and tested door
    status working.

--- SmartFridge/app/src/main/java/ubicomp/cs/uml/edu/smartfridge/SensorActivity.java
+++ SmartFridge/app/src/main/java/ubicomp/cs/uml/edu/smartfridge/SensorActivity.java
@@ -7,10 +7,12 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -25,6 +27,9 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     //Variable Declaration
     private SensorManager sensorManager = null;
 
+    private TextView Banner;
+    private TextView Status;
+
     //Accelerometer Values
     private TextView accX;
     private TextView accY;
@@ -35,6 +40,13 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private TextView magY;
     private TextView magZ;
 
+    private int Orientation;
+
+    //yaw pitch and roll
+    private double rmYaw;
+    private double rmPitch;
+    private double rmRoll;
+
     //Sensors
     private Sensor AccSensor;
     private Sensor MagSensor;
@@ -44,11 +56,16 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private float[] lastMagnetometer = new float[3];
     private boolean hasAccelerometerSet = false;
 
-    private float[] magField = new float[9];
+    private float[] mRotMat = new float[9];
     private float[] MagOrientation = new float[3];
 
     //Low Pass Filter Values
     static final float ALPHA = 0.2f;
+
+    //Open Event
+    private float doorVel;
+    private float doorVelCurrent;
+    private float doorVelLast;
 
     //Time Stamp
     private String timeStamp;
@@ -73,12 +90,19 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
             magY = (TextView) findViewById(R.id.textView05);
             magZ = (TextView) findViewById(R.id.textView06);
 
+            Banner = (TextView) findViewById(R.id.textView2);
+            Status = (TextView) findViewById(R.id.textView);
 
             accX.setText("initial test string");
 
             timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
             outputFile =Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+timeStamp+".csv";
             writer = new StringBuilder();
+
+            //initialising Door Open Close Calculators
+            doorVel = 0.00f;
+            doorVelCurrent = 0.01f;
+            doorVelLast = 0.01f;
 
         }
 
@@ -100,6 +124,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     protected void onResume() {
         super.onResume();
         Log.d("OnRESUME", "called");
+        //Registering Sensors
         hasAccelerometerSet = false;
         AccSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, AccSensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -112,6 +137,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         super.onStop();
         Log.d("OnSTOP", "called");
         hasAccelerometerSet = false;
+        //Un-Registering Sensors
         sensorManager.unregisterListener(this, AccSensor);
         sensorManager.unregisterListener(this, MagSensor);
     }
@@ -119,6 +145,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        //No Settings Menu As of Now
         if (id == R.id.action_settings) {
             return true;
         }
@@ -137,30 +164,84 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
             try{
                 NumberFormat nf = NumberFormat.getNumberInstance();
                 nf.setMaximumFractionDigits(1);
-
+                Orientation = getResources().getConfiguration().orientation;
                 //Getting the Accelerometer Values
                 if (event.sensor == AccSensor){
-                    lastAcclerometer = lowpass(event.values,lastAcclerometer);
+
+                    lastAcclerometer[0]=(lastAcclerometer[0]*2+event.values[0])*0.33334f;
+                    lastAcclerometer[1]=(lastAcclerometer[1]*2+event.values[1])*0.33334f;
+                    lastAcclerometer[2]=(lastAcclerometer[2]*2+event.values[2])*0.33334f;
 
                     accX.setText("x: "+ nf.format(lastAcclerometer[0]));
                     accY.setText("y: "+ nf.format(lastAcclerometer[1]));
                     accZ.setText("z: " + nf.format(lastAcclerometer[2]));
-                    hasAccelerometerSet = true;
-                    writeToFile(writer.toString());
 
+                    hasAccelerometerSet = true;
                 }
 
                 //Getting the Magnetometer Values
                 else if (event.sensor == MagSensor) {
-                    lastMagnetometer = lowpass(event.values,lastMagnetometer);
+
                     if (hasAccelerometerSet) {
-                        SensorManager.getRotationMatrix(magField, null, lastAcclerometer, lastMagnetometer);
-                        SensorManager.getOrientation(magField, MagOrientation);
-                        magX.setText("x: " + nf.format(Math.toDegrees(MagOrientation[0])));
-                        magY.setText("y: "+nf.format(Math.toDegrees(MagOrientation[1])));
-                        magZ.setText("z: "+nf.format(Math.toDegrees(MagOrientation[2])));
-                        writer.append(nf.format(lastAcclerometer[0]) + "," + nf.format(lastAcclerometer[1]) + "," + nf.format(lastAcclerometer[1]) + ","+nf.format(Math.toDegrees(MagOrientation[0])) + "," + nf.format(Math.toDegrees(MagOrientation[1])) + "," + nf.format(Math.toDegrees(MagOrientation[2])) + "\n");
+                        SensorManager.getRotationMatrix(mRotMat, null, lastAcclerometer, lastMagnetometer);
+                        SensorManager.getOrientation(mRotMat, lastMagnetometer);
+                        lastMagnetometer[0]=(lastMagnetometer[0]*1+event.values[0])*0.5f;
+                        lastMagnetometer[1]=(lastMagnetometer[1]*1+event.values[1])*0.5f;
+                        lastMagnetometer[2]=(lastMagnetometer[2]*1+event.values[2])*0.5f;
+                        lastMagnetometer = lowpass(event.values,lastMagnetometer);
+
                     }
+                }
+                //Detect the door status
+                if((event.sensor == MagSensor) || (event.sensor == AccSensor))
+                {
+                    float newMat[]=new float[16];
+                    SensorManager.getRotationMatrix(newMat, null, lastAcclerometer, lastMagnetometer);
+
+                    if(Orientation==0||Orientation==2){
+                        SensorManager.remapCoordinateSystem(newMat,SensorManager.AXIS_X*-1, SensorManager.AXIS_MINUS_Y*-1,newMat);
+                    }
+                    else{
+                        SensorManager.remapCoordinateSystem(newMat,SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X,newMat);
+                    }
+
+                    //Calcuting Yaw, Pitch and Roll using Eulers Angles Equation https://en.wikipedia.org/wiki/Euler_angles
+                    rmYaw = Math.toDegrees(Math.atan2(newMat[4], newMat[0]));
+                    rmPitch = Math.toDegrees(Math.acos(-newMat[8]));
+                    rmRoll = Math.toDegrees(Math.atan2(newMat[9], newMat[10]));
+
+                    magX.setText("yaw: " + (int) Math.round(rmYaw));
+                    magY.setText("pitch: " + (int) Math.round(rmPitch));
+                    magZ.setText("roll: " + (int) Math.round(rmRoll));
+
+
+                    writer.append(nf.format(lastAcclerometer[0]) + "," + nf.format(lastAcclerometer[1]) + "," + nf.format(lastAcclerometer[1]) + ","+nf.format(rmYaw) + "," + nf.format(rmPitch) + "," + nf.format(rmRoll) + "\n");
+
+                    //Logging into External Csv File 
+                    //writeToFile(writer.toString());
+
+                    //Calculating the Door Open Event
+                    doorVelLast = doorVelCurrent;
+                    doorVelCurrent = (float) Math.sqrt(rmYaw*rmYaw+rmPitch*rmPitch+rmRoll*rmRoll - SensorManager.GRAVITY_EARTH);
+                    float d = doorVelCurrent - doorVelLast;
+                    doorVel = doorVel * 0.09f + doorVelCurrent * 0.01f;
+
+                   //Toast.makeText(getApplicationContext(), String.valueOf(doorVel), Toast.LENGTH_SHORT).show();
+
+                    Status.setText(String.valueOf(Math.round(doorVel) ));
+
+                    //Call to Video Capture Methodology
+                    if (((rmPitch >= -6.0)||(rmPitch  <= 6.0)) && (doorVel >= 2))
+                    {
+                        Banner.setText(R.string.doorOpen);
+                        //@Anvesh Add Function Call to Take Video
+                    }
+                    else{
+                        Banner.setText(R.string.doorClosed);
+                        //@Anvesh Add Function Call to Stop Video
+                    }
+
+
                 }
                 //Error Management
                 else {
@@ -186,6 +267,8 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         return sensorOutput;
     }
 
+
+    //Implementation to Write the Sensor Data into External Storage.
     private void writeToFile(String data) {
         try {
             FileOutputStream fout = new FileOutputStream (new File(outputFile).getAbsolutePath(),true);
